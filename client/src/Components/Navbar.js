a75f2f3932061d6987fa2cf8ff648ca1364ee45d
Mario Rodriguez <mario.crv208@gmail.com>
2022-10-30 03:48:18 -0600

    Carga y descarga de archivos lista

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import { saveAs } from 'file-saver';
 
-function NavBar(){
-    const [numPage, setnumPages] = useState(null)
-    const [pageNumber, setPageNumber] = useState(1)
-
-    function onDocumentLoadSucces({numPage}){
-        setnumPages(numPage);
-        setPageNumber(1);
+function NavBar(props){
+    //Cosas para creacion de archivos y lectura de archivos
+    const createFile = () =>{
+        const blob = new Blob([props.value], {type: 'text/plain;charset=utf-8'})
+        saveAs( blob, '../ArchivosSalida/Salida.olc')
     }
+
     
 
     return(
@@ -20,8 +19,8 @@ function NavBar(){
             </div>
             <nav class="navbar bg-dark" >
                 <form class="container-fluid justifyContent">
-                    <button class="btn btn-outline-success me-2" type="button" style={{backgroundColor: '#E36246', borderColor: 'black', color: 'black'}}>Crear archivos</button>
-                    <button class="btn btn-outline-success me-2" type="button" style={{backgroundColor: '#E3B346', borderColor: 'black', color: 'black'}}>Abrir archivos</button>
+                    <button class="btn btn-outline-success me-2" type="button" style={{backgroundColor: '#E36246', borderColor: 'black', color: 'black'}} onClick={createFile}>Crear archivos</button>
+                    {props.comp}
                     <button class="btn btn-outline-success me-2" type="button" style={{backgroundColor: '#99E346', borderColor: 'black', color: 'black'}}>Guardar el archivo</button>
                     <button class="btn btn-outline-success me-2" type="button" style={{backgroundColor: '#466AE3', borderColor: 'black', color: 'black'}}>Tabla de Símbolos General</button>
                 </form>
--- client/src/Pages/Index.js
+++ client/src/Pages/Index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import NavBar from "../Components/Navbar";
 import TextArea from "../Components/TextArea"
 import Footer from "../Components/footer";
@@ -6,6 +6,8 @@ import Service from "../Services/Service";
 import { RViewerTrigger, RViewer} from 'react-viewerjs';
 
 function Index(){
+    const elementVariable = useRef(null)
+
     const [ value, setValue ] = useState("")
     const [ response, setResponse ] = useState("")
 
@@ -34,15 +36,38 @@ function Index(){
         require("../Imagenes/err.jpg")
     ]
 
+    const readFile = (e) => {
+        const file = e.target.files[0];
+        if(!file) return 
+
+        const fileReader = new FileReader()
+
+        fileReader.readAsText(file)
+
+        fileReader.onload=()=>{
+            elementVariable.current.childNodes[0].childNodes[0].childNodes[0].value = fileReader.result
+        }
+
+        
+        fileReader.onerror=()=>{
+            console.log(fileReader.error)
+        }
+    }
+
+    const botonCargar = <div class="mb-3">
+        <label for="formFileMultiple" class="form-label">Multiple files input example</label>
+        <input class="btn btn-outline-success me-2" type="file" id="formFileMultiple" multiple style={{backgroundColor: '#E3B346', borderColor: 'black', color: 'black'}} onChange={readFile} ></input>
+    </div>
+
     return(
         
         <>    
-        <NavBar></NavBar>
+        <NavBar value={value} comp = {botonCargar}></NavBar>
         <body style={{backgroundColor: '#6373C5', height: '100%', width: '100vw'}}>    
             <div style={{backgroundColor: '#6373C5'}}>
                 <h1 style={{textAlign: 'center', marginBottom: '35px'}}>BIENVENIDO A NUESTRA PAGINA :D</h1>
                 <div
-                class="container-fluid" style={{display: 'flex', wrap: 'wrap', direction: 'row', backgroundColor: '#6373C5'}}>
+                ref={elementVariable} class="container-fluid" style={{display: 'flex', wrap: 'wrap', direction: 'row', backgroundColor: '#6373C5'}}>
                     <TextArea id='input' text="INPUT" handlerChange={changeText} comp = {buttonEjecutar} comp2 = {buttonLimpiar}></TextArea>
                     <TextArea text ="CONSOLE" value={response}></TextArea>
                 </div>
@@ -51,7 +76,6 @@ function Index(){
                     <RViewer imageUrls={imagenes}>
                         <div style={{display: 'flex', marginTop:"35px"}}>
                             {imagenes.map((imagen, index)=>{
-                                        console.log(index)
                                 return(
                                     <RViewerTrigger index={index}>
                                         <img src={imagen} style={{width: '90%', height: '100%', marginLeft:'5%', border:'1px solid black'}}></img>
@@ -63,10 +87,9 @@ function Index(){
                 </div>
                 <div style={{backgroundColor: '#6373C5'}}>
                     <h2 style={{textAlign: 'center', marginBottom: '35px', marginTop:"35px"}}>REPORTE ERRORES</h2>
-                    <RViewer imageUrls={imagenes}>
+                    <RViewer imageUrls={imagenesErr}>
                         <div style={{display: 'flex', marginTop:"35px"}}>
-                            {imagenes.map((imagen, index)=>{
-                                        console.log(index)
+                            {imagenesErr.map((imagen, index)=>{
                                 return(
                                     <RViewerTrigger index={index}>
                                         <img src={imagen} style={{width: '90%', height: '100%', marginLeft:'5%', border:'1px solid black'}}></img>
